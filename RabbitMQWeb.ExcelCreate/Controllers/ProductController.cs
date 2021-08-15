87165ff7bb54d2ca9cda66d999b9c8aea395e336
yildiraygemuk <yildiraygemuk@gmail.com>
2021-08-15 12:45:33 +0300

    firstcommit

---
+++ RabbitMQWeb.ExcelCreate/Controllers/ProductController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RabbitMQWeb.ExcelCreate.Models;
+using RabbitMQWeb.ExcelCreate.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RabbitMQWeb.ExcelCreate.Controllers
+{
+    [Authorize]
+    public class ProductController : Controller
+    {
+        private readonly AppDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RabbitMQPublisher _rabbitMQPublisher;
+        public ProductController(AppDbContext context, UserManager<IdentityUser> userManager, RabbitMQPublisher rabbitMQPublisher)
+        {
+            _context = context;
+            _rabbitMQPublisher = rabbitMQPublisher;
+            _userManager = userManager;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public async Task<IActionResult> CreateProductExcel()
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            var fileName = $"product-excel-{Guid.NewGuid().ToString().Substring(1, 9)}";
+            UserFile userfile = new()
+            {
+                UserId = user.Id,
+                FileName = fileName,
+                FileStatus = FileStatus.Creating
+            };
+            await _context.UserFiles.AddAsync(userfile);
+            await _context.SaveChangesAsync();
+            //rabbitMQ mesaj gönder
+            _rabbitMQPublisher.Publish(new Shared.CreateExcelMessage()
+            {
+                FileId = userfile.Id
+            });
+            TempData["StartCreatingExcel"] = true;
+            return RedirectToAction(nameof(Files));
+        }
+
+        public async Task<IActionResult> Files()
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+
+            return View(await _context.UserFiles.Where(x => x.UserId == user.Id).OrderByDescending(x=> x.Id).ToListAsync());
+        }
+    }
+}
---
+++ Shared/CreateExcelMessage.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Shared
+{
+    public class CreateExcelMessage
+    {
+        public int FileId { get; set; }
+    }
+}
